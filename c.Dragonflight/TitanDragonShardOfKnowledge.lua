b9fe49c007c7926f1ddc37d68bdf32db9eb80c2d
Protuhj <ihatespam234@gmail.com>
2024-05-11 20:35:04 -0400

    Updates from master branch

--- c.Dragonflight/TitanDragonShardOfKnowledge.lua
+++ c.Dragonflight/TitanDragonShardOfKnowledge.lua
@@ -1,16 +1,16 @@
 --[[
-Description: This plugin is part of the "Titan Panel [Currencies] Multi" addon. It shows your Renascent Shadowflame.
+Description: This plugin is part of the "Titan Panel [Currencies] Multi" addon. It shows your Dragon Shard of Knowledge counts.
 Site: https://www.curseforge.com/wow/addons/titan-panel-currencies-multi
 Author: Canettieri
 Special Thanks to Protuhj.
 --]]
 
 local _, L = ...;
-local ID = "TITAN_RENSFLM"
-local CURRENCY_ID = 2533
+local ID = "TITAN_DRAGONSHARD"
+local ITEM_ID = 191784
 
-L:CreateSimpleCurrencyPlugin({
-	currencyId = CURRENCY_ID,
+L:CreateSimpleItemPlugin({
+	itemId = ITEM_ID,
 	titanId = ID,
 	noCurrencyText = L["DragonFOnly"],
 	expName = L["mDragonflight"],
